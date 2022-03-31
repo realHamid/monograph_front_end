314438391ad21898dfea9279cc669567e486b386
realHamid <1122hamidjan@gmail.com>
2022-03-31 09:29:00 +0430

    work on insert District

--- src/component/district/District.js
+++ src/component/district/District.js
@@ -1,7 +1,70 @@
 import React , {Component} from "react";
 import ListDistrict from "./ListDistrict";
+import axios from "../axios/axios";
 
 class District extends Component{
+
+    state =  {
+        list : [],
+        listProvince : []
+    };
+
+
+    getListOfProvince = () => {
+
+        let auth = {
+            'token'  : "b58ac01c6c7a9fb5ffd1a5d9c7d68955" ,
+            'api_token' : JSON.parse(localStorage.getItem('user-data')).api_token
+        };
+
+        axios.post('/district/listProvince',auth).then((response) => {
+
+            this.setState((oldState,props)  => {
+                return {
+                    ...oldState,
+                    listProvince : response.data
+                }
+            });
+
+        }).catch((err) => {
+            console.log(err);
+        });
+
+    }
+
+    componentDidMount() {
+        this.getListOfProvince();
+    }
+
+    submitForm = e => {
+
+        e.preventDefault();
+
+        let form = document.getElementById('district_from');
+        let formData = new FormData(form);
+
+
+        formData.append('token' , 'b58ac01c6c7a9fb5ffd1a5d9c7d68955');
+        formData.append('api_token' , JSON.parse(localStorage.getItem('user-data')).api_token);
+
+
+
+        axios.post('/district',formData).then((response) => {
+
+            document.getElementById('name').value = '';
+            document.getElementById('date').value = '';
+            document.getElementById('note').value = '';
+            
+            // this.getList();
+
+        }).catch((err) => {
+            console.log(err);
+        });
+
+    }
+
+
+
 
     render() {
         return(
@@ -17,7 +80,7 @@ class District extends Component{
                                 <h4 className="card-title"><span className="bfont"> ثبت اطلاعات  </span></h4>
                                 <p className="card-description"><span className="bfont"> مشخصات ولسوالی وارد نموده ثبت نماید ! </span>
                                 </p>
-                                <form className="forms-sample">
+                                <form className="forms-sample" id="district_from" onSubmit={this.submitForm}>
 
 
                                     <div className="form-group row mr-sm-4">
@@ -28,38 +91,38 @@ class District extends Component{
                                     </div>
 
                                     <div className="form-group row mr-sm-4">
-                                        <label htmlFor="exampleInputUsername2" className="col-sm-2 col-form-label bfont" >   ولایت  <span className="text-danger">*</span> </label>
+                                        <label htmlFor="province_id" className="col-sm-2 col-form-label bfont" >   ولایت  <span className="text-danger">*</span> </label>
                                         <div className="col-sm-7">
-                                            <select className="form-control form-control-sm  bfont">
+                                            <select id="province_id" required name="province_id" className="form-control form-control-sm  bfont">
                                                 <option value=""> یک ولایت را انتخاب نماید  </option>
-                                                <option> هرات  </option>
-                                                <option> کابل </option>
-                                                <option> مزار </option>
-                                                <option> قندهار </option>
-                                                <option> فراه </option>
-                                                <option> بادغیس  </option>
+                                                {
+                                                    this.state.listProvince.map( item  => (
+                                                        <option value={item.id}> { item.name } </option>
+                                                    ))
+                                                }
+
                                             </select>
                                         </div>
                                     </div>
 
                                     <div className="form-group row mr-sm-4">
-                                        <label htmlFor="exampleInputUsername2" className="col-sm-2 col-form-label bfont" >  نام  <span className="text-danger">*</span> </label>
+                                        <label htmlFor="name" className="col-sm-2 col-form-label bfont" >  نام  <span className="text-danger">*</span> </label>
                                         <div className="col-sm-7">
-                                            <input type="text" className="form-control form-control-sm " id="exampleInputMobile"  />
+                                            <input type="text" id="name" required name="name" className="form-control form-control-sm "  />
                                         </div>
                                     </div>
 
                                     <div className="form-group row mr-sm-4">
-                                        <label htmlFor="exampleInputUsername2" className="col-sm-2 col-form-label bfont" >  تاریخ ثبت  <span className="text-danger">*</span> </label>
+                                        <label htmlFor="date" className="col-sm-2 col-form-label bfont" >  تاریخ ثبت  <span className="text-danger">*</span> </label>
                                         <div className="col-sm-7">
-                                            <input type="date" className="form-control form-control-sm  bfont"  placeholder="" />
+                                            <input type="date" id="date" required name="date" className="form-control form-control-sm  bfont"  placeholder="" />
                                         </div>
                                     </div>
 
                                     <div className="form-group row mr-sm-4">
-                                        <label htmlFor="exampleInputUsername2" className="col-sm-2 col-form-label bfont" >  توضیحات   </label>
+                                        <label htmlFor="note" className="col-sm-2 col-form-label bfont" >  توضیحات   </label>
                                         <div className="col-sm-7">
-                                            <textarea className="form-control form-control-sm  bfont" style={{height: '120px'}} ></textarea>
+                                            <textarea id="note" name="note" className="form-control form-control-sm  bfont" style={{height: '120px'}} ></textarea>
                                         </div>
                                     </div>
                                     <br />
